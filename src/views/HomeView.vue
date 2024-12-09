7e7bbe8fc9a1a0246c46cbc4f8b7eb1a43a96e09
mehmetefeozcan <ozcanmehmetefe@gmail.com>
2024-12-09 11:14:04 +0300

    add tailwind & daisyui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="w-full h-[100vh] flex items-center justify-center  ">
+
+    <button className="btn">Button</button>
+    <button className="btn btn-neutral">Neutral</button>
+    <button className="btn btn-primary">Primary</button>
+    <button className="btn btn-secondary">Secondary</button>
+    <button className="btn btn-accent">Accent</button>
+    <button className="btn btn-ghost">Ghost</button>
+    <button className="btn btn-link">Link</button>
+  </div>
 </template>
