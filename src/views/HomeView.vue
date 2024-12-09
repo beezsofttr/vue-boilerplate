8db517c868b894fa97ce0654fc511d36ee769257
Günalp Uysal <gunalp@beezsoft.com>
2024-12-09 11:17:51 +0300

    htfx

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div class="w-full h-[100vh] flex items-center justify-center  ">
-
+  <div class="w-full h-[100vh] flex items-center justify-center">
     <button className="btn">Button</button>
     <button className="btn btn-neutral">Neutral</button>
     <button className="btn btn-primary">Primary</button>
